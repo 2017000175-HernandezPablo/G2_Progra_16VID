01cd9bf889072817baae81ddb7d6752f9d61312d
2017000175-HernandezPablo <p.hernandez.5cdb@gmail.com>
2026-04-16 12:02:40 -0600

    Ejer3_visual

---
+++ Ejer3_Visual.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("RANGO DE ESTUDIANTE");
+        Console.WriteLine("Ingrese una letra (A, B, C, D, F)");
+        char letra = Char.ToUpper(Console.ReadKey(). KeyChar);
+
+        Console.WriteLine();
+
+        switch (letra)
+        {
+            case 'A':
+                Console.WriteLine("EXELENTE");
+                break;
+            case 'B':
+                Console.WriteLine("BUENO");
+                break;
+            case 'C':
+                Console.WriteLine("REGULAR");
+                break;
+            case 'D':
+                Console.WriteLine("DEFICIENTE");
+                break;
+            case 'F':
+                Console.WriteLine("REPROBADO");
+                break;
+            default:
+                Console.WriteLine("Ingrese letra valida");
+                break;
+        }
+    }
+}
